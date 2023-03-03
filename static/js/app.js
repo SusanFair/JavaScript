c1e6a8648780cb6d0e328c5e20059c91f8c3185f
SusanFair <sfair@live.ca>
2023-03-02 22:27:44 -0500

    Challenge Updates

--- static/js/app.js
+++ static/js/app.js
@@ -28,19 +28,13 @@ function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
     var changedElement = d3.select(this);
-    console.log(changedElement);
-
+  
     // 4b. Save the value that was changed as a variable.
     let elementValue = changedElement.property("value");
-    console.log('This is elementValue $elementValue');
-    console.log(elementValue);
 
     // 4c. Save the id of the filter that was changed as a variable.
     let filterId = changedElement.attr("id");
-    console.log("This is filterId");
-    console.log(filterId);
-    console.log("This is filters");
-    console.log(filters);
+
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
     if (elementValue) {
@@ -48,15 +42,10 @@ function updateFilters() {
     }
     else {
         delete filters[filterId];
-        // console.log("This is Step 5");
-        // console.log(changedElement);
-        // console.log(filters)
     }
-
 
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
-
 }
 
 // 7. Use this function to filter the table when data is entered.
@@ -65,85 +54,19 @@ function filterTable() {
     //  8. Set the filtered data to the tableData.
     // filteredDate is for a start filled with the whole original data set tableData
     let filteredData = tableData;
-    console.log("This is the whole filteredData")
-    console.log(tableData);
+ 
     // 9. Loop through all of the filters and keep any data that
     //    matches the filter values
-
-    // for (const input in filters) {
-    //     console.log("This is the for loop")
-    //     console.log(filters[input]);
-    //     console.log(input);
-    //     if (input) {
-    //         filteredData = filteredData.filter(row => row.input === filters[input]);
-    //    }
-    //   }
-    //   console.log("Print filteredData")
-    //   console.log(filteredData)
-
-
     Object.entries(filters).forEach((filter) => {
-        console.log("This is within Object.entries")
-        console.log(filter)
-        console.log(filteredData);
-        console.log(filteredData.filter(row => row.city === 'benton'))
         filteredData = filteredData.filter(row => row[filter[0]] === filter[1]);
-        console.log("Print filteredData");
-        console.log(filteredData);
-
     })
 
-
-
-
-
-
-
-    //   Object.entries(filters).forEach((filter) => {
-    //     console.log(filter)
-    //     console.log(filter[1])
-    //     filteredData = filteredData.filter(row => row.filter[0] === filter[1]);
-    //     console.log("Print filteredData");
-    //     console.log(filteredData);
-
-    //   })
-
-
-
-
-    //   for (const input in filters) {
-    //     console.log("This is the for loop")
-    //     console.log(filters[input]);
-    //     if (input) {
-    //         filteredData = filteredData.filter(row => row.input === input);
-    //         console.log("Print filterValue");
-    //         console.log(input);
-    //     }
-    //   }
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
-    console.log("Run step buildTable")
 }
 
-
-// data.forEach((weatherReport) => {
-//     let row = tbody.append("tr");
-//     Object.entries(weatherReport).forEach(([key, value]) => {
-//       let cell = row.append("td");
-//       cell.text(value);
-//     });
-//   });
-
-
-// if (date) {
-//     filteredData = filteredData.filter(row => row.datetime === date);
-// }
-
-
-
-
-//   // 2. Attach an event to listen for changes to each filter
-//   // Was filter-btn, changes to 
+// 2. Attach an event to listen for changes to each filter
+// Was filter-btn, changes to change
 d3.selectAll("input").on("change", updateFilters);
 
 
